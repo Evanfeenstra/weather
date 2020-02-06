6934c6436156dd20d1b70284dc9a797a88802b5b
Evan Feenstra <evan@evanfeenstra.com>
2020-02-06 15:59:43 -0800

    t

--- api/index.js
+++ api/index.js
@@ -8,7 +8,6 @@ module.exports = async function(req, res) {
   const {lat, lon} = req.query
   try {
     const url = `${root}/${key}/${lat},${lon}`
-    console.log(url)
     const r = await fetch(url)
     const json = await r.json()
     res.status(200).send(json)
@@ -16,9 +15,3 @@ module.exports = async function(req, res) {
     res.status(500).send('fail: '+e.message)
   }
 }
-
-/*
-
-curl "https://weatherchart-k5e6f1e2s.now.sh/api?lat=47.6062&lon=-122.3321"
-
-*/
