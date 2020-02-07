2b77b82226192faddf90d81ad0f177a16af000fb
Evan Feenstra <evan@evanfeenstra.com>
2020-02-07 09:53:46 -0800

    now

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import {Input, Button} from 'antd'
 import {Bar} from 'react-chartjs-2'
 import * as moment from 'moment'
+const ButtonGroup = Button.Group;
 
 const context = React.createContext()
 
 function App() {
   const [state, setState] = useState({
     searchTerm:'',
+    mode:'hourly'
   })
   return <context.Provider value={{
     ...state,
@@ -21,9 +23,10 @@ function App() {
   </context.Provider>
 }
 
+const modes=['hourly','daily']
 function Header(){
   const ctx = useContext(context)
-  const {loading, searchTerm} = ctx
+  const {loading, searchTerm, mode} = ctx
   return <header className="App-header">
     <Input 
       value={searchTerm} disabled={loading}
@@ -38,20 +41,34 @@ function Header(){
       disabled={!searchTerm} loading={loading}>
       Search
     </Button>
+    <ButtonGroup style={{marginLeft:5,display:'flex'}}>
+      {modes.map(m=> <Button style={{height:'3rem'}} 
+        type={mode===m?'primary':'default'}
+        onClick={()=> ctx.set({mode:m})}>
+        {cap(m)}
+      </Button>)}
+    </ButtonGroup>
   </header>
 }
 
 function Body(){
   const ctx = useContext(context)
-  const {error, weather} = ctx
+  const {error, weather, mode} = ctx
   let data
   if(weather){
     console.log(weather)
     data = {
-      labels: weather.hourly.data.map(d=> moment(d.time*1000).format('dd hh:mm') ),
+      labels: weather[mode].data.map(d=> {
+        let format = 'ddd'
+        if(mode==='hourly') format='dd hh:mm'
+        return moment(d.time*1000).format(format)
+      }),
       datasets: [{
         label:'Temperature',
-        data: weather.hourly.data.map(d=>d.temperature),
+        data: weather[mode].data.map(d=>{
+          if(mode==='hourly') return d.temperature
+          else return (d.temperatureHigh+d.temperatureLow)/2
+        }),
         backgroundColor: 'rgba(132,99,255,0.2)',
         borderColor: 'rgba(132,99,255,1)',
         hoverBackgroundColor: 'rgba(132,99,255,0.4)',
@@ -95,3 +112,7 @@ async function search({searchTerm, set}){
 }
 
 export default App;
+
+function cap(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
